1b7e24c2ac0540c0ade89085a349ccd7d6481c6c
Duccio Rocchini <ducciorocchini@gmail.com>
2024-08-02 11:48:11 +0200

    Update ecochange.R

--- ecochange.R
+++ ecochange.R
@@ -1,12 +1,14 @@
+# Intro to the package
+
+# install.packages("ecochange")
+library(ecochange)
+library(viridis)
+
 # Original code: lk-erz
 
 # Package paper by Wilson Lara and colleagues:
 # ecochange: An R-package to derive ecosystem change indicators from freely available earth observation products
 # https://doi.org/10.1111/2041-210X.13986
-
-# install.packages("ecochange")
-library(ecochange)
-library(viridis)
 
 ## RasterBrick of structural Essential Biodiversity Variables
 ## covering the extent of a location in the northern Amazon basin
@@ -15,10 +17,18 @@ path <- system.file('amazon.grd', package = 'ecochange')
 amazon <- brick(path)
 plot(amazon, col=viridis(100))
 
-# check standardization of legends
-
 # print available variables
 names(amazon)
+
+# plotting temp
+plot(amazon, 3:6, col=cividis(100))
+
+# standardization of legends by zlim
+plot(amazon, 3:6, col=cividis(100), zlim=c(0,255))
+
+# difference
+diftc = amazon[[6]] - amazon[[3]]  
+plot(diftc, col=magma(100))
 
 
 ##### echanges: Ecosystem changes #####
@@ -32,7 +42,6 @@ def <- echanges(amazon,                 # Stack
                 eco = 'TC',             # eco: subset of layers representing the target ecosystem variables
                 change = 'lossyear')    # Name of the layer of ecosystem changes 
                   
-
 # Method 'plot.echanges' allows comparing rasters using a common scale bar:
 plot.echanges(def, main="Changes in tree canopy cover [%]", 
                 labels=c("2000", "2005", "2010", "2015"))
@@ -47,33 +56,3 @@ plot.EBVstats(st_amazon,
               sub = 'Northern Amazon',
               fill = 'Layer')
 
-# gaugeIndicator: compute ecosystem areas
-am_areas <- gaugeIndicator(def,
-                           mc.cores = 2)
-plot.Indicator(am_areas)
-
-#-------------------------------------------------------------------------------
-# get country polygons with "getGADM"
-# download respective remote sensing data.
-# variables contain surface water, forest change, tree cover data
-# for the entire list, see: listGP()
-listGP()
-
-# example Colombia
-# getGADM
-muni <- getGADM(unit.nm = NULL,
-                level = 2, country = "COL",
-                path = tempdir())
-head(muni)
-
-## Polygon of the Colombian municipality of Cartagena del Chaira:
-load(system.file('cchaira_roi.RData', package = 'ecochange'))
-## A Global Surface Water layer ('seasonality') which covers the
-## extent of the polygon is retrieved:
-rsp_cchaira <- getrsp(cchaira_roi,
-                      lyrs = 'seasonality', mc.cores = 2, path = tempdir())
-file.exists(rsp_cchaira)
-
-## use getWRS to download Landsat data!
-wrs_cchaira <- getWRS(cchaira_roi) # wait for the output
-plot(wrs_cchaira) # odd
