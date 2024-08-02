3177946cb995cd6281723456535be1ef912fbff8
Duccio Rocchini <ducciorocchini@gmail.com>
2024-08-02 12:02:43 +0200

    Create ggridge.R

---
+++ ggridge.R
@@ -0,0 +1,17 @@
+# ggridges: Ridgeline Plots in 'ggplot2'
+
+# install.packages("ggridges")
+packagelist <- c("ggridges", "ggplot2")
+lapply(packagelist, require) #check!
+
+library(ggridges)
+library(ggplot2)
+
+# temperature variations
+ggplot(lincoln_weather, aes(x = `Mean Temperature [F]`, y = Month, fill = stat(x))) +
+  geom_density_ridges_gradient(scale = 3, rel_min_height = 0.01) +
+  scale_fill_viridis_c(name = "Temp. [F]", option = "C") +
+  labs(title = 'Temperatures in Lincoln NE in 2016')
+
+# super!
+
