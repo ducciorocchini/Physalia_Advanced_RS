ef272eda13a611ad34bb2681f0225e2999ebc782
Duccio Rocchini <ducciorocchini@gmail.com>
2024-08-26 16:40:53 +0200

    Update colorblindness.R

--- Code/colorblindness.R
+++ Code/colorblindness.R
@@ -2,17 +2,38 @@
 
 # Check for color blindness
 
+# install.packages("colorblindcheck")
+library(devtools)
+# devtools::install_github("clauswilke/colorblindr")
+# devtools::install_github("ducciorocchini/cblindplot")
+library(colorblindcheck)
+library(colorblindr)
+library(ggplot2)
+library(patchwork)
+library(imageRy)
+library(terra)
+
 ## colorblindcheck
 
 rainbow_pal <- rainbow(7)
-colorblindcheck::palette_check(rainbow_pal, plot=TRUE)
+palette_check(rainbow_pal, plot=TRUE)
+
+rainbow_pal <- rainbow(100)
+palette_check(rainbow_pal, plot=TRUE)
 
 ## colorblindr
 
-library(ggplot2)
+# install_github("colorblindr")
+
 explot <- ggplot(iris, aes(Sepal.Length, fill=Species)) +
 geom_density(alpha=0.7)
 colorblindr::cvd_grid(explot)
+
+explot2 <- ggplot(iris, aes(Sepal.Length, fill = Species)) + 
+  geom_density(alpha = 0.7) + scale_fill_OkabeIto()
+explot2
+
+explot + explot2
 
 ## cblindplot
 rainbowinput = "rainbow.png"
